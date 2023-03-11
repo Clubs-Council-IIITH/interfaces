dcde038dc5497d968642cdc2818b1e813be7d5e7
v15hv4 <vishva2912@gmail.com>
2023-03-12 00:15:00 +0530

    Consistent query names

--- queries.py
+++ queries.py
@@ -8,7 +8,7 @@ from otypes import Info, SignedURL
 
 # fetch signed url from the files service
 @strawberry.field
-def getSignedURL(info: Info) -> SignedURL:
+def signedUploadURL(info: Info) -> SignedURL:
     user = info.context.user
 
     # make request to files api
@@ -25,5 +25,5 @@ def getSignedURL(info: Info) -> SignedURL:
 
 # register all queries
 queries = [
-    getSignedURL,
+    signedUploadURL,
 ]
